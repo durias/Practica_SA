bb42475408782fc5bd4413f90f29aa84d2f40b2a
Danilo Urías <d14urias@gmail.com>
2020-12-05 23:46:38 -0600

    nueva prueba
    prueba

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Practica 1 Software avanzado prueba')
+  res.send('Practica 1 Software avanzado poderosa prueaba')
 })
 
 app.listen(port, () => {
