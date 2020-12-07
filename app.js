8f80828d411e35deeca7013d1da1f41c36223925
Danilo Urías <d14urias@gmail.com>
2020-12-07 00:32:29 -0600

    Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Practica 1 Software avanzado vacaciones dec 2020 siuuuu')
+  res.send('Practica 1 Software avanzado vacaciones dec 2020000')
 })
 
 app.listen(port, () => {
