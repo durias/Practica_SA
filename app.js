6b6ea0e3084f3d043928ebc0fc9e10817d07d84f
Danilo Urías <d14urias@gmail.com>
2020-12-06 23:58:58 -0600

    Diciembre

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Practica 1 Software avanzado poderosa prueaba')
+  res.send('Practica 1 Software avanzado Diciembre 2020')
 })
 
 app.listen(port, () => {
