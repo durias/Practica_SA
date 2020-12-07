093161c5346f2ef14af5c8d781d9b71f165026c1
Danilo Urías <d14urias@gmail.com>
2020-12-07 15:52:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var path    = require("path");
+
+app.use('/style', express.static('style'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 app.get('/', (req, res) => {
   res.send('Practica 1 Software avanzado vacaciones dec 2020000')
 })
@@ -9,4 +16,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
